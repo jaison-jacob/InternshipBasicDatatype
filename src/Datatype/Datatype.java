da31c4fb0695be03c84aa9b8269fe35e5ad2d615
Jaison Jacob <jaisonjai250795@gmail.com>
2020-11-08 19:37:31 +0530

    it is datatype

---
+++ src/Datatype/Datatype.java
@@ -0,0 +1,25 @@
+package Datatype;
+
+public class Datatype {
+
+	public static void main(String arg[]){
+		
+		//assingvalue
+		
+		int number = 34;
+		String string = "it is string";
+		float Float = 87f;
+		boolean bool = true;
+		
+		//output
+		
+		System.out.println(number);
+		System.out.println(string);
+		System.out.println(Float);
+		System.out.println(bool);
+		
+	}
+	
+}
+
+
